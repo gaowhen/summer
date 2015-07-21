43262e0b75eaa822ff714c357c01a1e92c0b2bfc
gaowhen <gaowhen.com@gmail.com>
2015-07-21 15:54:14 +0800

    add baidu analytics code

---
+++ fe/static/js/_ga.js
@@ -0,0 +1,9 @@
+/**
+ * Created by gaowhen on 15/7/21.
+ */
+
+var hm = document.createElement("script")
+hm.src = "//hm.baidu.com/hm.js?001a4013ae38f6fb9d157d1ee374b41c"
+
+var s = document.getElementsByTagName("script")[0]
+s.parentNode.insertBefore(hm, s)
---
+++ fe/static/js/common.js
@@ -0,0 +1,5 @@
+/**
+ * Created by gaowhen on 15/7/21.
+ */
+
+require('./_ga')
