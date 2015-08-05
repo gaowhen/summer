8cc7b1fd4aac4958650ca13132f39b9ca0b2e792
gaowhen <gaowhen.com@gmail.com>
2015-08-05 12:06:30 +0800

    ga code

--- fe/static/js/_ga.js
+++ fe/static/js/_ga.js
@@ -3,25 +3,29 @@
  */
 
 if (!$('body').hasClass('dev')) {
+	// baidu
 	var hm = document.createElement('script')
 	hm.src = '//hm.baidu.com/hm.js?001a4013ae38f6fb9d157d1ee374b41c'
 
 	var s = document.getElementsByTagName('script')[0]
 	s.parentNode.insertBefore(hm, s)
 
-	(function (i, s, o, g, r, a, m) {
-		i['GoogleAnalyticsObject'] = r;
-		i[r] = i[r] || function () {
-					(i[r].q = i[r].q || []).push(arguments)
-				}, i[r].l = 1 * new Date();
-		a = s.createElement(o),
-				m = s.getElementsByTagName(o)[0];
-		a.async = 1;
-		a.src = g;
-		m.parentNode.insertBefore(a, m)
-	})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+	// google
+	window['GoogleAnalyticsObject'] = 'ga'
 
-	ga('create', 'UA-11027068-1', 'auto');
-	ga('send', 'pageview');
+	window['ga'] = window['ga'] ||
+		function () {
+			(window['ga'].q = window['ga'].q || []).push(arguments)
+		}
 
+	window['ga'].l = 1 * new Date()
+
+	var a = document.createElement('script')
+	a.src = '//www.google-analytics.com/analytics.js'
+	var sc = document.getElementsByTagName('script')[0]
+
+	sc.parentNode.insertBefore(a, sc)
+
+	ga('create', 'UA-11027068-1', 'auto')
+	ga('send', 'pageview')
 }
