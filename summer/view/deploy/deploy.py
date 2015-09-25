396e5ee8d832fa4b5d570b9f02548c281c0fced8
gaowhen <gaowhen.com@gmail.com>
2015-09-25 15:55:37 +0800

    use git cmd directly

--- summer/view/deploy/deploy.py
+++ summer/view/deploy/deploy.py
@@ -9,27 +9,32 @@ bp = Blueprint('deploy', __name__)
 
 @bp.route('/deploy', methods=['POST'])
 def deploy():
-	repo_path = './ghpages'
-	repo_url = current_app.config['REPO']
+    repo_path = './ghpages'
+    repo_url = current_app.config['REPO']
 
-	repo = Repo(repo_path)
+    repo = Repo(repo_path)
 
-	if (repo.bare):
-		repo = Repo.init(repo_path, bare=True)
+    if (repo.bare):
+        repo = Repo.init(repo_path, bare=True)
 
-	index = repo.index
+    #index = repo.index
 
-	for diff in index.diff(None):
-		print diff.a_blob
-		index.add([diff.a_blob.name])
-	#index.add([diff.a_blob.name for diff in index.diff(None)])
+    #for diff in index.diff(None):
+    #    if diff.a_blob:
+    #        index.add([diff.a_blob.name])
+    #    elif diff.b_blob:
+    #        index.remove([diff.b_blob.name])
+    ##index.add([diff.a_blob.name for diff in index.diff(None)])
 
-	for untracked in repo.untracked_files:
-		print 'untracked'
-		print untracked
-		index.add([untracked])
+    #for untracked in repo.untracked_files:
+    #    print 'untracked'
+    #    print untracked
+    #    index.add([untracked])
 
-	index.commit('new commit')
-	repo.git.push()
+    #index.commit('new commit')
 
-	return jsonify(r=True)
+    repo.git.add(all=True)
+    repo.git.commit('new commit')
+    repo.git.push()
+
+    return jsonify(r=True)
